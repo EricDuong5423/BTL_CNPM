750ea2dd01ca5fa4ae2116ef647d718609f9b81c
EricDuong5423 <tuduong05042003@gmail.com>
2024-11-24 16:52:45 +0700

    setting cors for access

--- server/app.js
+++ server/app.js
@@ -2,12 +2,23 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const printerRoutes = require("./routes/printerRoutes");
+const cors = require("cors");
 
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
